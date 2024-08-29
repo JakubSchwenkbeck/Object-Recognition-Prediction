cb8f17a8c62883417726c272cc25e24cd7602771
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-08-29 22:45:37 +0200

    Added First fields of Conv Layer

---
+++ src/AI_Model/Layers/ConvolutionalLayer.java
@@ -0,0 +1,19 @@
+package AI_Model.Layers;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ConvolutionalLayer {
+
+    private long SEED;
+
+    private List<double[][]> _filters;
+
+    private int _filterSize;
+
+    private int _stepSize;
+
+    private int _inLength;
+
+
+}
