ada7cdf47e8b426893612ad42698ca40d7e0f5eb
Jakub <168542356+JakubSchwenkbeck@users.noreply.github.com>
2024-08-30 18:54:43 +0200

    Added new Helper Functons

--- src/AI_Model/Layers/Layer.java
+++ src/AI_Model/Layers/Layer.java
@@ -12,29 +12,29 @@ public abstract class Layer {
     /**
      * The next layer in the network.
      */
-    protected Layer _nextLayer;
+    protected Layer nextLayer;
 
     /**
      * The previous layer in the network.
      */
-    protected Layer _previousLayer;
+    protected Layer previousLayer;
 
     /**
      * Gets the next layer in the network.
      *
      * @return The next layer.
      */
-    public Layer get_nextLayer() {
-        return _nextLayer;
+    public Layer getNextLayer() {
+        return nextLayer;
     }
 
     /**
      * Sets the next layer in the network.
      *
-     * @param _nextLayer The next layer to set.
+     * @param nextLayer The next layer to set.
      */
-    public void set_nextLayer(Layer _nextLayer) {
-        this._nextLayer = _nextLayer;
+    public void setNextLayer(Layer nextLayer) {
+        this.nextLayer = nextLayer;
     }
 
     /**
@@ -42,17 +42,17 @@ public abstract class Layer {
      *
      * @return The previous layer.
      */
-    public Layer get_previousLayer() {
-        return _previousLayer;
+    public Layer getPreviousLayer() {
+        return previousLayer;
     }
 
     /**
      * Sets the previous layer in the network.
      *
-     * @param _previousLayer The previous layer to set.
+     * @param previousLayer The previous layer to set.
      */
-    public void set_previousLayer(Layer _previousLayer) {
-        this._previousLayer = _previousLayer;
+    public void setPreviousLayer(Layer previousLayer) {
+        this.previousLayer = previousLayer;
     }
 
     /**
@@ -61,7 +61,7 @@ public abstract class Layer {
      * @param input The input data as a list of 3D matrices.
      * @return The output of the layer as a 1D array.
      */
-    public abstract double[] getOutput(List<double[][]> input);
+    public abstract double[] computeOutput(List<double[][]> input);
 
     /**
      * Calculates the output of the layer given a 1D array as input.
@@ -69,14 +69,14 @@ public abstract class Layer {
      * @param input The input data as a 1D array.
      * @return The output of the layer as a 1D array.
      */
-    public abstract double[] getOutput(double[] input);
+    public abstract double[] computeOutput(double[] input);
 
     /**
      * Performs backpropagation given the derivative of the loss with respect to the output.
      *
      * @param dLdO The derivative of the loss with respect to the output as a 1D array.
      */
-    public abstract void backPropagation(double[] dLdO);
+    public abstract void backpropagate(double[] dLdO);
 
     /**
      * Performs backpropagation given the derivative of the loss with respect to the output
@@ -84,7 +84,7 @@ public abstract class Layer {
      *
      * @param dLdO The derivative of the loss with respect to the output as a list of 3D matrices.
      */
-    public abstract void backPropagation(List<double[][]> dLdO);
+    public abstract void backpropagate(List<double[][]> dLdO);
 
     /**
      * Gets the length of the output.
@@ -112,7 +112,7 @@ public abstract class Layer {
      *
      * @return The total number of elements in the output.
      */
-    public abstract int getOutputElements();
+    public abstract int getTotalOutputElements();
 
     /**
      * Converts a list of 3D matrices to a 1D array.
@@ -120,7 +120,7 @@ public abstract class Layer {
      * @param input The list of 3D matrices to convert.
      * @return The resulting 1D array.
      */
-    public double[] matrixToVector(List<double[][]> input) {
+    public double[] convertMatrixToVector(List<double[][]> input) {
         int length = input.size();
         int rows = input.get(0).length;
         int cols = input.get(0)[0].length;
@@ -131,8 +131,7 @@ public abstract class Layer {
         for (int l = 0; l < length; l++) {
             for (int r = 0; r < rows; r++) {
                 for (int c = 0; c < cols; c++) {
-                    vector[i] = input.get(l)[r][c];
-                    i++;
+                    vector[i++] = input.get(l)[r][c];
                 }
             }
         }
@@ -143,29 +142,28 @@ public abstract class Layer {
     /**
      * Converts a 1D array to a list of 3D matrices.
      *
-     * @param input The 1D array to convert.
+     * @param input  The 1D array to convert.
      * @param length The number of 3D matrices.
-     * @param rows The number of rows in each 3D matrix.
-     * @param cols The number of columns in each 3D matrix.
+     * @param rows   The number of rows in each 3D matrix.
+     * @param cols   The number of columns in each 3D matrix.
      * @return The resulting list of 3D matrices.
      */
-    public List<double[][]> vectorToMatrix(double[] input, int length, int rows, int cols) {
-        List<double[][]> out = new ArrayList<>();
-        int i = 0;
+    public List<double[][]> convertVectorToMatrix(double[] input, int length, int rows, int cols) {
+        List<double[][]> output = new ArrayList<>();
+        int index = 0;
 
         for (int l = 0; l < length; l++) {
             double[][] matrix = new double[rows][cols];
 
             for (int r = 0; r < rows; r++) {
                 for (int c = 0; c < cols; c++) {
-                    matrix[r][c] = input[i];
-                    i++;
+                    matrix[r][c] = input[index++];
                 }
             }
 
-            out.add(matrix);
+            output.add(matrix);
         }
 
-        return out;
+        return output;
     }
 }
