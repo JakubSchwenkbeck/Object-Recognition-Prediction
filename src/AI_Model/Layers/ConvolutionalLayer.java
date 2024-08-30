67a54b8b485090e3477d8510a9b35d08ad7b53f5
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-08-30 18:49:24 +0200

    Added ConvolutionForwardPass to ConvLayer

--- src/AI_Model/Layers/ConvolutionalLayer.java
+++ src/AI_Model/Layers/ConvolutionalLayer.java
@@ -15,5 +15,66 @@ public class ConvolutionalLayer {
 
     private int _inLength;
 
+    private int _inRows;
 
-}
+    private int _inCols;
+
+    private double learningRate;
+
+
+    private List<double[][]> _lastInput;
+
+    public ConvolutionalLayer(int _filterSize, int _stepsize, int _inLength, int _inRows, int _inCols, long SEED, int numFilters, double learningRate) {
+        this._filterSize = _filterSize;
+        this._stepSize = _stepsize;
+        this._inLength = _inLength;
+        this._inRows = _inRows;
+        this._inCols = _inCols;
+        this.SEED = SEED;
+        this.learningRate = learningRate;
+
+         generateRandomFilters(numFilters);
+    }
+
+    private void generateRandomFilters(int numFilters){
+        List<double[][]> filters = new ArrayList<>();
+        Random random = new Random(SEED);
+
+        for(int n = 0; n < numFilters; n++) {
+            double[][] newFilter = new double[_filterSize][_filterSize];
+
+            for(int i = 0; i < _filterSize; i++){
+                for(int j = 0; j < _filterSize; j++){
+
+                    double value = random.nextGaussian();
+                    newFilter[i][j] = value;
+                }
+            }
+
+            filters.add(newFilter);
+
+        }
+
+        _filters = filters;
+    }
+
+    public List<double[][]> convolutionForwardPass(List<double[][]> list){
+        _lastInput = list;
+
+        List<double[][]> output = new ArrayList<>();
+
+        for (int m = 0; m < list.size(); m++){
+            for(double[][] filter : _filters){
+                output.add(convolve(list.get(m), filter, _stepSize));
+            }
+
+        }
+
+        return output;
+
+    }
+
+
+
+
+    }
