1924a2602fbfee8634745347772ff155d5becf58
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-08-30 18:43:01 +0200

    Implemented simple Matrix util

--- src/Util/MatrixUtil.java
+++ src/Util/MatrixUtil.java
@@ -17,4 +17,45 @@ public class MatrixUtil {
         return res;
     }
 
+    public static double[] add(double[] a, double[] b){
+        // going with the assumption of square shaped, not null matrices
+        double[] res = new double[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+
+                res[i] = a[i] + b[i];
+            }
+
+
+
+
+        return res;
+    }
+
+    public static double[][] mul(double[][] a, double scalar){
+        // going with the assumption of square shaped, not null matrices
+        double[][] res = new double[a.length][a[0].length];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                res[i][j] = a[i][j] * scalar;
+            }
+
+        }
+
+
+        return res;
+    }
+    public static double[] mul(double[] a, double scalar){
+        // going with the assumption of square shaped, not null matrices
+        double[] res = new double[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+
+            res[i] = a[i] *scalar;
+        }
+
+
+        return res;
+    }
 }
