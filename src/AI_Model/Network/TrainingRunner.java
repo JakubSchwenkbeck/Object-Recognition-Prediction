0747d4b204b27502eebddb08f3c8d7ecd6762ef1
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-08-31 17:51:07 +0200

    PascalVOCDataLoader for Image preprocessing and loading

--- src/AI_Model/Network/TrainingRunner.java
+++ src/AI_Model/Network/TrainingRunner.java
@@ -16,7 +16,11 @@ public class TrainingRunner {
 
     public static void main(String[] args) {
      NeuralNetwork net =   NeuralNetwork.buildNetwork(256,256,1,123);
-     List<PascalVOCDataLoader> XML = PascalVOCDataLoader.loadDir("C:/Users/jakub/IdeaProjects/PascalVOC_Data/VOC2012_test/Annotations");
+     String xmlDirPath = "C:/Users/jakub/IdeaProjects/PascalVOC_Data/VOC2012_test/Annotations";
+        String imageDirPath = "C:/Users/jakub/IdeaProjects/PascalVOC_Data/VOC2012_test/JPEGImages";
+        Size targetSize = new Size(224, 224); // Adjust to your CNN input size
 
+        List<PascalVOCDataLoader> dataLoaders =PascalVOCDataLoader.loadDir(xmlDirPath);
+        List<TrainingSample> trainingSamples = PascalVOCDataLoader.loadAndPreprocessImages(dataLoaders, imageDirPath, targetSize);
     }
 }
