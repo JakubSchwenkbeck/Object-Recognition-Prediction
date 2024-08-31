3bbb7979dbf735a70d29330cccf374d343d00592
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-08-31 16:55:35 +0200

    PascalVOCDataLoader and TrainingRunner improvements

--- src/AI_Model/Network/TrainingRunner.java
+++ src/AI_Model/Network/TrainingRunner.java
@@ -11,23 +11,28 @@ import java.util.List;
 
 
 public class TrainingRunner {
-    /*
+
+
+
     public static void main(String[] args) {
+        /*
         String imageDir = "path/to/images";
         String annotationDir = "path/to/annotations";
 
         PascalVOCDataLoader dataLoader = new PascalVOCDataLoader();
         List<Mat> images = dataLoader.loadImages(imageDir);
-        List<ObjectAnnotation> annotations = dataLoader.loadAnnotations(annotationDir);
+        List<ObjectAnnotation> annotations = dataLoader.(annotationDir);
 
         DataPreprocessor.prepareTrainingData(images, annotations);
 
         // Initialize and train your neural network
         NeuralNetwork network = NeuralNetwork.buildNetwork(224, 224, 1.0, 1234L); // Example parameters
         network.train(images, annotations);
-        
+
         // Evaluate the network
         float accuracy = network.test(images, annotations);
         System.out.println("Accuracy: " + accuracy);
-    }*/
+
+         */
+    }
 }
